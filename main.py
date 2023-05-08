3c69904007c5c2bf8adc0c92ecc214324d644190
MusicMel0n <78413704+MusicMel0n@users.noreply.github.com>
2023-05-08 10:02:13 +0100

    Worked on Python and got a graph to display

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import statistics
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def main():
-    print("t")
+    food_data = pd.read_csv(r"data\01CN.csv")
+    restaurant_data = pd.read_csv(r"data\11CN.csv")
+    food_data = food_data.iloc[2:86]
+    print(food_data)
+    food_data = food_data.drop(food_data.iloc[:, 2:], axis=1)
+    food_data["Food and non-alcoholic beverages"]=food_data["Food and non-alcoholic beverages"].str.replace(',', '')
+    food_data["Food and non-alcoholic beverages"]=food_data["Food and non-alcoholic beverages"].astype(int)
+    print(food_data.dtypes)
+    food_data.plot(ylabel = "Money Spent (£)", x = "Time period and codes")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
