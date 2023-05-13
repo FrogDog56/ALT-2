34e56225a5b04266a9c184f7608cd6ff08c9907b
MusicMel0n <78413704+MusicMel0n@users.noreply.github.com>
2023-05-13 16:43:47 +0100

    Fixed full range of x values not showing up
    + Minor Tweaks
    + Graph now shows full range of X values

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def main():
+    xValues = []
+
     food_data = pd.read_csv(r"data\01CN.csv")
     restaurant_data = pd.read_csv(r"data\11CN.csv")
     food_data = food_data.iloc[2:86]
-    print(food_data)
     food_data = food_data.drop(food_data.iloc[:, 2:], axis=1)
     food_data["Food and non-alcoholic beverages"]=food_data["Food and non-alcoholic beverages"].str.replace(',', '')
     food_data["Food and non-alcoholic beverages"]=food_data["Food and non-alcoholic beverages"].astype(int)
-    print(food_data.dtypes)
-    food_data.plot(ylabel = "Money Spent (£)", x = "Time period and codes")
+    
+    for i in range(1, len(food_data["Time period and codes"]) + 1):
+        xValues.append(i)
+
+    plt.plot(food_data["Time period and codes"], food_data["Food and non-alcoholic beverages"], label="Food and non-alcoholic beverages")
+    plt.xlabel("Time period and codes")
+    plt.ylabel("Money Spent (£)")
+    plt.title("Money Spent Compared To Time Period")
+    plt.legend(loc="upper right")
+    plt.xticks(xValues, food_data["Time period and codes"], rotation="vertical")
+    plt.get_current_fig_manager().window.state("zoomed")
     plt.show()
 
 
