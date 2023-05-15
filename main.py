1a099f57afd5b1e0e2d0eb21f20fc805850d021c
MusicMel0n <78413704+MusicMel0n@users.noreply.github.com>
2023-05-15 10:03:39 +0100

    DID STUFF!!!!!!

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def clean_up(data, column):
+    data = data.iloc[2:86]
+    data = data.drop(data.iloc[:, 2:], axis=1)
+    data[column]=data[column].str.replace(",", "")
+    data[column]=data[column].astype(int)
+    return data
+
 def main():
     xValues = []
 
     food_data = pd.read_csv(r"data\01CN.csv")
     restaurant_data = pd.read_csv(r"data\11CN.csv")
-    food_data = food_data.iloc[2:86]
-    food_data = food_data.drop(food_data.iloc[:, 2:], axis=1)
-    food_data["Food and non-alcoholic beverages"]=food_data["Food and non-alcoholic beverages"].str.replace(',', '')
-    food_data["Food and non-alcoholic beverages"]=food_data["Food and non-alcoholic beverages"].astype(int)
-    
+    clothing_data = pd.read_csv(r"data\03CN.csv")
+    food_data = clean_up(food_data, "Food and non-alcoholic beverages")
+    restaurant_data = clean_up(restaurant_data, "Restaurant and hotels")
+    clothing_data = clean_up(clothing_data, "Clothing and footwear")
+
     for i in range(1, len(food_data["Time period and codes"]) + 1):
         xValues.append(i)
 
     plt.plot(food_data["Time period and codes"], food_data["Food and non-alcoholic beverages"], label="Food and non-alcoholic beverages")
+    plt.plot(food_data["Time period and codes"], restaurant_data["Restaurant and hotels"], label="Restaurants and hotels")
+    plt.plot(food_data["Time period and codes"], clothing_data["Clothing and footwear"], label="Clothing and footwear")
     plt.xlabel("Time period and codes")
     plt.ylabel("Money Spent (£)")
     plt.title("Money Spent Compared To Time Period")
