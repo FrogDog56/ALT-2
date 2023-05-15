e7d2fd6e1a0066e659c9b99031d9cd8971aca7cd
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2023-05-15 08:27:47 +0100

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,9 +1,17 @@
 /* my own custom js html injector to avoid pasting the same page over and over */
-const regex_pattern = /<body[^>]*>((.|[\n\r])*)<\/body>/im;
+const regex = /<body[^>]*>((.|[\n\r])*)<\/body>/im;
+const pages = {
+  0: "intro.html",
+  1: "plan.html",
+  2: "design.html",
+  3: "create.html",
+  4: "evaluate.html",
+  5: "reflection.html"
+}
 
 async function loadHtml() {
-  const response = await fetch("pages/introduction.html");
-  const text = (await response.text()).match(regex_pattern);
+  const response = await fetch("pages/" + pages[0]);
+  const text = (await response.text()).match(regex);
   document.getElementById("inject").innerHTML = text[0];
 }
 
